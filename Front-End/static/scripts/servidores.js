fb1a11f8c8a27cf6016fdde6f0cb1a667767c4ec
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-02 21:56:04 -0300

    modificamos servidor.js
    ahora emite una alerta avisando qeu el nombre del servidor esta en uso

--- Front-End/static/scripts/servidores.js
+++ Front-End/static/scripts/servidores.js
@@ -32,76 +32,6 @@ const serverNameElement = document.querySelector('.server_name');
 serverNameElement.querySelector('p').textContent = 'Servidor';
 
 ////////////// READ SERVIDORES //////////////////////////
-// function cargarServidores(idUsuario) {
-//   // Limpiar la lista de servidores existentes
-//   serverList.innerHTML = '';
-
-//   fetch(`http://127.0.0.1:5000/servidor/user/${idUsuario}`, {
-//       method: 'GET',
-//       credentials: 'include',
-//   })
-//   .then((response) => {
-//       if (!response.ok) {
-//           throw new Error('Error al obtener la lista de servidores');
-//       }
-//       return response.json();
-//   })
-//   .then((data) => {
-//       // La respuesta del servidor con la lista de servidores se encuentra en 'data'
-//       const servers = data;
-
-//       if (Array.isArray(data) && data.length > 0) {
-//         noShowServers();}
-
-//       // Recorrer la lista de servidores y crear elementos HTML para cada uno de ellos
-//       servers.forEach((server) => {
-//           const newServerItem = document.createElement('div');
-//           newServerItem.classList.add('profile1');
-
-//           // Almacenar el id_servidor como un atributo personalizado
-//           newServerItem.dataset.serverId = server.id_servidor;
-
-//           const serverIcon = document.createElement('p');
-//           serverIcon.textContent = server.nombre[0];
-
-//           const whiteLine = document.createElement('div');
-//           whiteLine.classList.add('white_line');
-
-//           const hoverText = document.createElement('div');
-//           hoverText.classList.add('hover');
-//           hoverText.textContent = server.nombre;
-
-//           newServerItem.appendChild(serverIcon);
-//           newServerItem.appendChild(whiteLine);
-//           newServerItem.appendChild(hoverText);
-
-//           // Agregar el nuevo servidor a la lista de servidores en el frontend
-//           serverList.appendChild(newServerItem);
-
-//           // Agregar un evento click al nuevo servidor
-//           newServerItem.addEventListener('click', (event) => {
-//               serverId = event.currentTarget.dataset.serverId;
-  
-              
-//               cargarCanales(serverId);
-
-//               // Obtener el elemento con la clase "server_name"
-//               const serverNameElement = document.querySelector('.server_name');
-
-//               // Cambiar el contenido del elemento a la nueva palabra
-//               serverNameElement.querySelector('p').textContent = server.nombre;
-
-//               toggleChannelColumnVisibility();
-    
-//               // Imprime el valor en la consola o haz lo que necesites con él
-//               console.log('Valor de data-server-id:', serverId);
-//           });
-//       });
-//   })
-//   .catch((error) => {
-//       console.error(error.message); // Maneja errores si ocurrieron
-//   });
-// }
 async function cargarServidores(idUsuario) {
   try {
     // Limpiar la lista de servidores existentes
@@ -177,38 +107,6 @@ async function cargarServidores(idUsuario) {
 
 
   ////////////////// CREATE SERVIDORES //////////////////////
-// function crearServidor(serverData) {
-
-
-//   console.log(serverData);
-
-
-//       // Realiza la solicitud HTTP POST al backend
-//       fetch('http://127.0.0.1:5000/servidor/', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(serverData),
-//         credentials: 'include',
-//       })
-//         .then((response) => {
-//           if (!response.ok) {
-//             throw new Error('Error al crear el servidor');
-//           }
-//           return response.json();
-//         })
-//         .then((data) => {
-//           // La respuesta del servidor se encuentra en 'data'
-//           console.log(data.message); // Muestra el mensaje de éxito en la consola
-//           cargarServidores(serverData.id_usuario)
-
-//           // Aquí puedes realizar cualquier otra acción después de crear el servidor con éxito
-//         })
-//         .catch((error) => {
-//           console.error(error.message); // Maneja errores si ocurren
-//     });
-//   }
 
 async function crearServidor(serverData) {
   try {
@@ -225,23 +123,23 @@ async function crearServidor(serverData) {
     });
 
     if (!response.ok) {
-      throw new Error('Error al crear el servidor');
+      if (response.status === 400) {
+        const errorData = await response.json();
+        console.error(errorData.message); // Muestra el mensaje de error del backend
+        alert(errorData.message);
+      } else {
+        throw new Error('Error al crear el servidor');
+      }
+    } else {
+      const data = await response.json();
+      console.log(data.message); // Muestra el mensaje de éxito en la consola
+      await cargarServidores(serverData.id_usuario);
+      // Aquí puedes realizar cualquier otra acción después de crear el servidor con éxito
     }
-
-    const data = await response.json();
-
-    // La respuesta del servidor se encuentra en 'data'
-    console.log(data.message); // Muestra el mensaje de éxito en la consola
-    await cargarServidores(serverData.id_usuario);
-
-    // Aquí puedes realizar cualquier otra acción después de crear el servidor con éxito
   } catch (error) {
-    console.error(error.message); // Maneja errores si ocurren
+    console.error(error.message); // Maneja otros errores si ocurren
   }
 }
-
-
-
 
 
 
@@ -316,23 +214,6 @@ async function buscarServidores(buscador) {
     console.error(error.message); // Maneja errores si ocurrieron
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     //if (Array.isArray(data) && data.length > 0) {

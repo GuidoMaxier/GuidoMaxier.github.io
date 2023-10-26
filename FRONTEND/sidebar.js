155a6de8556b37279e8fa7699aae98e07e22eb6e
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-10-26 02:33:02 -0300

    ultimos
    se hicieron retoque en la js y en html

--- FRONTEND/sidebar.js
+++ FRONTEND/sidebar.js
@@ -17,78 +17,26 @@ for (var i = 0; i < arrows.length; i++) {
 }
 
 
-// Obtén todos los elementos del menú
-const menuItems = document.querySelectorAll('.nav-list li');
 
-// Obtén el iframe
-const iframe = document.getElementById('content-frame');
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuLinks = document.querySelectorAll(".nav-list a");
 
-// Agrega un evento de clic a cada elemento del menú
-menuItems.forEach((menuItem) => {
-menuItem.addEventListener('click', function() {
-// Obtén la URL de data-src del elemento del menú actual
-const dataSrc = this.getAttribute('data-src');
-if (dataSrc) {
-    // Cambia la URL del iframe
-    iframe.src = dataSrc;
-       }
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado de los enlaces.
+
+        const dataSrc = link.getAttribute("href"); // Obtiene la URL de destino.
+        const iframe = document.getElementById("content-frame");
+
+        if (dataSrc && iframe) {
+          iframe.src = dataSrc; // Carga la URL de destino en el iframe.
+        }
+      });
     });
-   });
+  });
 
 
 
 // se logea
-
-
-   document.addEventListener("DOMContentLoaded", function () {
-    const userSection = document.getElementById("userSection");
-    const loginLink = document.getElementById("loginLink");
-    const registerLink = document.getElementById("registerLink");
-
-    // Verificar si hay un usuario en la sesión
-    const usuarioLogeadoJSON = sessionStorage.getItem("usuarioLogeado");
-    if (usuarioLogeadoJSON) {
-        const usuarioLogeado = JSON.parse(usuarioLogeadoJSON);
-
-        // Mostrar información del usuario y botón de logout
-        loginLink.style.display = "none";
-        registerLink.style.display = "none";
-        userSection.style.display = "flex";
-        // Actualiza el avatar y nombre del usuario
-        document.getElementById("userAvatar").src = usuarioLogeado.avatar;
-        document.getElementById("userName").textContent = usuarioLogeado.nombre;
-
-        // Añade un evento para el botón de logout
-        document.getElementById("logoutButton").addEventListener("click", function () {
-            // Elimina el usuario de la sesión
-            sessionStorage.removeItem("usuarioLogeado");
-
-            // Redirige al usuario a la página de inicio de sesión
-            window.location.href = "login.html";
-        });
-    } else {
-        // No hay usuario logeado en la sesión
-        loginLink.style.display = "block";
-        registerLink.style.display = "block";
-        userSection.style.display = "none";
-    }
-
-
-    const menuToggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("menu");
-
-
-    menuToggle.addEventListener("click", function () {
-        menu.classList.toggle("active");
-    });
-
-    // Cierra el menú cuando se hace clic en cualquier otro lugar de la página
-    document.addEventListener("click", function (event) {
-        if (!menu.contains(event.target) && event.target !== menuToggle) {
-            menu.classList.remove("active");
-        }
-    });
-});
-
 
 
